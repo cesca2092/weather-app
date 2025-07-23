733d8bec964a737d8a45203516c8202d4eee48f3
cesar <cesca2092@gmail.com>
2025-07-23 10:54:43 -0500

    agregar form

---
+++ src/components/Form/Form.tsx
@@ -0,0 +1,61 @@
+import { useState, type ChangeEvent } from "react";
+import type { SearchType } from '../../types'
+import { countries } from "../../data/countries";
+import styles from './Form.module.css'
+
+export default function Form() {
+
+  const [search, setSearch] = useState<SearchType>({
+    city: '',
+    country: ''
+  })
+
+  const handleChange = (e: ChangeEvent<HTMLInputElement> | ChangeEvent<HTMLSelectElement>) => {
+    setSearch({
+      ...search,
+      [e.target.name]: e.target.value
+    })
+  }
+  return (
+    <form className={styles.form}>
+
+      <div className={styles.field}>
+        <label htmlFor='city'>Ciudad:</label>
+        <input
+          id='city'
+          type='text'
+          name='city'
+          placeholder='Ciudad'
+          value={search.city}
+          onChange={handleChange}
+        />
+      </div>
+
+      <div className={styles.field}>
+        <label htmlFor='country'>Pais:</label>
+        <select
+          id="country"
+          name="country"
+          value={search.country}
+          onChange={handleChange}
+        >
+          <option value={""}>-- Seleccione un Pais --</option>
+          {countries.map(country => (
+            <option
+              key={country.code}
+              value={country.code}
+            >
+              {country.name}
+            </option>
+          ))}
+        </select>
+      </div>
+
+      <input
+        className={styles.submit}
+        type="submit"
+        value={'Consultar Clima'}
+      />
+    </form>
+  )
+}
